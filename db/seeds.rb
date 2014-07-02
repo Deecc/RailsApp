d048d138c2b17f7d173a3f90756a11a888cf67d9
Deecc <decio.crf@gmail.com>
2014-07-02 09:10:29 -0300

    Add imagens e alterações de css

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,35 +7,78 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Product.delete_all
 
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+Product.create!(title: 'Use a Cabeça - Rails',
 	description: 
 	%{<p>
 		Ruby is the fastest growing and most exciting dynamic language
 		out there. If you need to get working programs delivered fast,
 		you should add Ruby to your toolbox
 		</p>},
-		image_url: 'ruby.jpg',
+		image_url: 'UseaCabecaRails.jpg',
 		price: 49.95, 
 		qtd: 10)
 
-Product.create!(title: 'Programming Java',
+Product.create!(title: 'Java 7 - Ensino didatico',
 	description: 
 	%{<p>
-		java is the fastest growing and most exciting dynamic language
+		Java is the fastest growing and most exciting dynamic language
 		out there. If you need to get working programs delivered fast,
 		you should add Java to your toolbox
 		</p>},
-		image_url: 'java.jpg',
+		image_url: 'Java7EnsinoDid.jpg',
 		price: 58.95, 
-		qtd: 0)
+		qtd: 3)
 
-Product.create!(title: 'Programming Python',
+Product.create!(title: 'Redes de Computadores',
 	description: 
 	%{<p>
-		Python is the fastest growing and most exciting dynamic language
-		out there. If you need to get working programs delivered fast,
-		you should add Python to your toolbox
+		Um livro de Gabriel Torres , sobre Redes.
+
 		</p>},
-		image_url: 'Python.jpg',
+		image_url: 'redesGabrielTorres.jpg',
 		price: 38.57, 
+		qtd: 9)
+
+Product.create!(title: 'Use a Cabeça - C#',
+	description: 
+	%{<p>
+		C# is the fastest growing and most exciting dynamic language
+		out there. If you need to get working programs delivered fast,
+		you should add C# to your toolbox
+
+		</p>},
+		image_url: 'UseaCabecaC.jpg',
+		price: 5.57, 
+		qtd: 6)
+
+Product.create!(title: 'Use a Cabeça - Web Design',
+	description: 
+	%{<p>
+		Web Design is the fastest growing and most exciting dynamic language
+		out there. If you need to get working programs delivered fast,
+		you should add Web Design to your toolbox
+
+		</p>},
+		image_url: 'UseaCabecaWebD.jpg',
+		price: 31.70, 
+		qtd: 6)
+
+Product.create!(title: 'Windows 7 para Leigos',
+	description: 
+	%{<p>
+		Windows 7, explicado de maneira fácil e compreensivel independente do seu nivel de conhecimento.
+
+		</p>},
+		image_url: 'Win7Leigos.jpg',
+		price: 22.99, 
 		qtd: 0)
+
+Product.create!(title: 'Engenharia de Software - 8ª Edição',
+	description: 
+	%{<p>
+		Engenharia de Software, Summerville.
+
+		</p>},
+		image_url: 'EngdeSoft.jpg',
+		price: 82.99, 
+		qtd: 5)
